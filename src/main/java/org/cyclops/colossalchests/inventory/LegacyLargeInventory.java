3f9dfb940eb7751ee3717082c076811287906329
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-08-11 03:43:36 +0800

    Update LegacyLargeInventory.java

--- src/main/java/org/cyclops/colossalchests/inventory/LegacyLargeInventory.java
+++ src/main/java/org/cyclops/colossalchests/inventory/LegacyLargeInventory.java
@@ -1,9 +1,5 @@
 package org.cyclops.colossalchests.inventory;
 
-/**
- * 兼容旧版 CyclopsCore LargeInventory 的替代类
- * 本质上和 LegacySimpleInventory 一样，只是名字不同
- */
 public class LegacyLargeInventory extends LegacySimpleInventory {
 
     public LegacyLargeInventory() {
