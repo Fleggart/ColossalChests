2ed5a7a27b68ff624304a7b5898b18e0081d3356
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-08-13 06:56:55 +0800

    Update ContainerUncolossalChest.java

--- src/main/java/org/cyclops/colossalchests/inventory/container/ContainerUncolossalChest.java
+++ src/main/java/org/cyclops/colossalchests/inventory/container/ContainerUncolossalChest.java
@@ -1,19 +1,11 @@
 package org.cyclops.colossalchests.inventory.container;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.ContainerHopper;
-import net.minecraft.inventory.Slot;
 import org.cyclops.colossalchests.tileentity.TileUncolossalChest;
 
-import java.util.List;
-import java.util.Map;
-
-// 移除 @ChestContainer 注解
 public class ContainerUncolossalChest extends ContainerHopper {
     public ContainerUncolossalChest(InventoryPlayer playerInventory, TileUncolossalChest tile) {
         super(playerInventory, tile, playerInventory.player);
     }
-
 }
