8ca79e8518f5e8bbe6d783d8e7bf3e6311d5e8f5
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-08-12 10:41:30 +0800

    Update Reference.java

--- src/main/java/org/cyclops/colossalchests/Reference.java
+++ src/main/java/org/cyclops/colossalchests/Reference.java
@@ -15,10 +15,8 @@ public class Reference {
     public static final String MOD_ID = "colossalchests";
     public static final String MOD_NAME = "ColossalChests";
     public static final String MOD_VERSION = "@VERSION@";
-    // 删除 MOD_BUILD_NUMBER
     public static final String MOD_CHANNEL = MOD_ID;
     public static final String MOD_MC_VERSION = "@MC_VERSION@";
-    // 删除 MOD_FINGERPRINT
     public static final String GA_TRACKING_ID = "UA-65307010-5";
     public static final String VERSION_URL = "https://raw.githubusercontent.com/CyclopsMC/Versions/master/1.12/ColossalChests.txt";
     
@@ -33,18 +31,13 @@ public class Reference {
     public static final String MODEL_PATH = "models/";
     
     // MOD ID's
-    // 删除 MOD_FORGE 和 MOD_FORGE_VERSION
     public static final String MOD_FORGE_VERSION_MIN = "14.23.5.2768";
     public static final String MOD_CYCLOPSCORE = "cyclopscore";
     public static final String MOD_CYCLOPSCORE_VERSION = "@CYCLOPSCORE_VERSION@";
     public static final String MOD_CYCLOPSCORE_VERSION_MIN = "1.3.0";
-    public static final String MOD_COMMONCAPABILITIES = "commoncapabilities";
-    public static final String MOD_COMMONCAPABILITIES_VERSION_MIN = "2.4.0";
-    // 删除 MOD_IRONCHEST
     
     // Dependencies
     public static final String MOD_DEPENDENCIES = 
             "required-after:forge@[14.23.5.2768,);" +
-            "required-after:cyclopscore@[1.3.0,);" +
-            "after:commoncapabilities@[2.4.0,);";
+            "required-after:cyclopscore@[1.3.0,);";
 }
