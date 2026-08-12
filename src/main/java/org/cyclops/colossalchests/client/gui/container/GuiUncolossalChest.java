786c1ed0f78a988c82586975f0e430f94364b86f
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-08-13 06:57:46 +0800

    Remove author comment from GuiUncolossalChest
    Removed author comment from GuiUncolossalChest class.

--- src/main/java/org/cyclops/colossalchests/client/gui/container/GuiUncolossalChest.java
+++ src/main/java/org/cyclops/colossalchests/client/gui/container/GuiUncolossalChest.java
@@ -4,9 +4,6 @@ import net.minecraft.client.gui.GuiHopper;
 import net.minecraft.entity.player.InventoryPlayer;
 import org.cyclops.colossalchests.tileentity.TileUncolossalChest;
 
-/**
- * @author rubensworks
- */
 public class GuiUncolossalChest extends GuiHopper {
     public GuiUncolossalChest(InventoryPlayer playerInv, TileUncolossalChest tile) {
         super(playerInv, tile);
