c2d4967659fee75e05f947f6f1b5d0bdd6a7d34b
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-08-13 08:09:19 +0800

    Update PropertyMaterial.java

--- src/main/java/org/cyclops/colossalchests/block/PropertyMaterial.java
+++ src/main/java/org/cyclops/colossalchests/block/PropertyMaterial.java
@@ -1,90 +1,25 @@
 package org.cyclops.colossalchests.block;
 
-import java.util.Optional;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import net.minecraft.block.properties.PropertyHelper;
+import net.minecraft.block.properties.PropertyEnum;
 import org.cyclops.colossalchests.Reference;
 import org.cyclops.cyclopscore.helper.L10NHelpers;
 
-import java.util.Collection;
 import java.util.Locale;
 
-/**
- * Material property for the chests
- * @author rubensworks
- */
-public class PropertyMaterial extends PropertyHelper<PropertyMaterial.Type> {
+public class PropertyMaterial extends PropertyEnum<PropertyMaterial.Type> {
 
-    private final Set<PropertyMaterial.Type> allowedValues;
+    // ========== 单例 ==========
+    private static final PropertyMaterial INSTANCE = new PropertyMaterial("material", Type.class, java.util.Arrays.asList(Type.values()));
 
-    protected PropertyMaterial(String name, Collection<PropertyMaterial.Type> values) {
-        super(name, Type.class);
-        this.allowedValues = Collections.unmodifiableSet(new HashSet<>(values));
+    private PropertyMaterial(String name, Class<Type> valueClass, java.util.Collection<Type> allowedValues) {
+        super(name, valueClass, allowedValues);
     }
 
-    public Collection<PropertyMaterial.Type> getAllowedValues() {
-        return this.allowedValues;
-    }
-
-    @Override
-    public Optional<Type> parseValue(String value) {
-        for (PropertyMaterial.Type type : allowedValues) {
-            if(type.toString().equalsIgnoreCase(value)) {
-                return Optional.empty();
-            }
-        }
-        return Optional.empty();
-    }
-
-    @Override
-    public String getName(PropertyMaterial.Type value) {
-        return value.toString().toLowerCase(Locale.ENGLISH);
-    }
-
-    /**
-     * Create a new PropertyStatus with all Enum constants of the given class.
-     * @param name The property name.
-     * @param clazz The property class.
-     * @return The property
-     */
-    public static PropertyMaterial create(String name, Class clazz) {
-        return create(name, clazz, t -> true);
-    }
-
-    /**
-     * Create a new PropertyStatus with all Enum constants of the given class.
-     * @param name The property name.
-     * @param clazz The property class.
-     * @param filter The filter for checking property values.
-     * @return The property
-     */
-    public static PropertyMaterial create(String name, Class clazz, Predicate filter) {
-        
-        return create(name, clazz, Arrays.stream(clazz.getEnumConstants())
-            .filter(filter)
-            .collect(Collectors.toList())
-         );
-        
-    }
-
-    /**
-     * Create a new PropertyStatus with all Enum constants of the given class.
-     * @param name The property name.
-     * @param clazz The property class.
-     * @param values The possible property values.
-     * @return The property
-     */
-    public static PropertyMaterial create(String name, Class clazz, Collection values) {
-        return new PropertyMaterial(name, values);
+    public static PropertyMaterial getInstance() {
+        return INSTANCE;
     }
 
     public enum Type {
-
         WOOD(1),
         COPPER(1.666),
         IRON(2),
@@ -95,7 +30,7 @@ public class PropertyMaterial extends PropertyHelper<PropertyMaterial.Type> {
 
         private final double inventoryMultiplier;
 
-        private Type(double inventoryMultiplier) {
+        Type(double inventoryMultiplier) {
             this.inventoryMultiplier = inventoryMultiplier;
         }
 
@@ -104,13 +39,12 @@ public class PropertyMaterial extends PropertyHelper<PropertyMaterial.Type> {
         }
 
         public String getLocalizedName() {
-            return L10NHelpers.localize("material." + Reference.MOD_ID + "." + toString().toLowerCase(Locale.ENGLISH));
+            return L10NHelpers.localize("material." + Reference.MOD_ID + "." + 
+                    toString().toLowerCase(Locale.ENGLISH));
         }
 
         public boolean isExplosionResistant() {
             return this == OBSIDIAN;
         }
-
     }
-
 }
